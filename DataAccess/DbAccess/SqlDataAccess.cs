9f1fe917a4a0d3092bbff913866f904498277096
DanilDutov <dutov@sfedu.ru>
2023-04-10 22:17:09 +0300

    Add project files.

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess.DbAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        _config = configuration;
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure,
+                                                     U parametrs, string connectionId = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+        return await connection.QueryAsync<T>(storedProcedure, parametrs, commandType: CommandType.StoredProcedure);
+    }
+
+
+    public async Task SaveData<T>(string storedProcedure,
+                                  T parametrs, string connectionId = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+        await connection.ExecuteAsync(storedProcedure, parametrs, commandType: CommandType.StoredProcedure);
+    }
+
+
+}
---
+++ MinimalApiWithDapper/Api.cs
@@ -0,0 +1,79 @@
+﻿
+
+namespace MinimalApiWithDapper;
+public static class Api
+{
+
+    public static void ConfigureApi(this WebApplication app)
+    {
+        app.MapGet("/Users", GetUsers);
+        app.MapGet("/Users/{Id}", GetUser);
+        app.MapPost("/Users", InsertUser);
+        app.MapPut("/Users", UpdateUser);
+        app.MapDelete("/Users", DeleteUser);
+    }
+
+    private static async Task<IResult> GetUsers(IUserData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetUsers());
+        }
+        catch(Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+
+    private static async Task<IResult> GetUser(int Id, IUserData data)
+    {
+        try
+        {
+            var res = await data.GetUser(Id);
+            if (res == null) return Results.NotFound();
+            return Results.Ok(res);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> InsertUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.InsertUser(user);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+    private static async Task<IResult> UpdateUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.UpdateUser(user);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+    private static async Task<IResult> DeleteUser(int Id, IUserData data)
+    {
+        try
+        {
+            await data.DeleteUser(Id);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+}
